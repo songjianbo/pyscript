fac7b910c51fc73a71651e5dfa52a6b113c60262
songjianbo <247645962@qq.com>
2017-11-27 16:31:18 +0800

    python代理

---
+++ proxy/proxy.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+
+#%%
+from bs4 import BeautifulSoup
+import requests
+import random
+
+def get_ip_list(url, headers):
+    web_data = requests.get(url, headers=headers)
+    soup = BeautifulSoup(web_data.text, 'lxml')
+    ips = soup.find_all('tr')
+    ip_list = []
+    for i in range(1, len(ips)):
+        ip_info = ips[i]
+        tds = ip_info.find_all('td')
+        if not tds[5].text.lower().startswith("https"):
+            ip_list.append(tds[5].text.lower()+'://' + tds[1].text + ':' + tds[2].text)
+    return ip_list
+
+def get_random_ip(ip_list):
+    proxy_ip = random.choice(ip_list)
+    proxies = {"http":proxy_ip}
+    return proxies
+
+if __name__ == '__main__':
+    url = 'http://www.xicidaili.com/nn/'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36'
+    }
+    ip_list = get_ip_list(url, headers=headers)
+    proxies = get_random_ip(ip_list)
+    print(proxies)
+    web_data = requests.get("http://www.whatismyip.com.tw/", headers=headers, proxies=proxies,timeout=2)
+    print(web_data.content.decode("utf-8"))
