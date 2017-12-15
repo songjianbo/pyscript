f7620a72829d6443d9ad27fbd647070fc8b1f79d
songjianbo <247645962@qq.com>
2017-12-15 17:24:49 +0800

    scrapy爬取腾讯社招信息

---
+++ Tencent/Tencent/spiders/tencent.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from Tencent.items import TencentItem
+
+
+class TencentSpider(scrapy.Spider):
+    name = 'tencent'
+    allowed_domains = ['tencent.com']
+    start_urls = ['http://hr.tencent.com/position.php?&start=0']
+
+    def parse(self, response):
+        if len(response.xpath("//a[@id='next' and @class='noactive']")) == 1:
+            return
+        node_list = response.xpath("//tr[@class ='even'] | // tr[@class ='odd']")
+        for node in node_list:
+            item = TencentItem()
+            item['positionName'] = node.xpath("./td[1]/a/text()").extract()[0]
+            item['positionLink'] = node.xpath("./td[1]/a/@href").extract()[0]
+            if len(node.xpath("./td[2]/text()")):
+                item['positionType'] = node.xpath("./td[2]/text()").extract()[0]
+            else:
+                item['positionType'] = ""
+            item['peopleNumber'] = node.xpath("./td[3]/text()").extract()[0]
+            item['workLocation'] = node.xpath("./td[4]/text()").extract()[0]
+            item['publishTime'] = node.xpath("./td[5]/text()").extract()[0]
+            yield item
+
+        url = response.xpath("//a[@id='next']/@href").extract()[0]
+        yield scrapy.Request("'http://hr.tencent.com/"+url)
