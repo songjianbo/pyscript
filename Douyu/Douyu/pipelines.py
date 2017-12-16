22aaa60475b84f9195771c6b288be3391c15fc3a
songjianbo <247645962@qq.com>
2017-12-16 20:29:33 +0800

    scrapy爬取斗鱼图片并重命名

---
+++ Douyu/Douyu/pipelines.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import scrapy
+from scrapy.pipelines.images import ImagesPipeline
+import os
+from settings import IMAGES_STORE as images_store
+
+
+class DouyuPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        imagelink = item['imagelink']
+        yield scrapy.Request(imagelink)
+
+    def item_completed(self, results, item, info):
+        image_path = [x['path'] for ok, x in results if ok]
+        old_path = os.path.join(images_store, image_path[0])
+        new_path = os.path.join(images_store, item['nickname'] + ".jpg")
+        if os.path.exists(old_path):
+            os.rename(old_path, new_path)
+        return item
